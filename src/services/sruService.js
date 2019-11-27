1f274f7ca6513f67004800ab031b8d8c334aa2d1
natlibfi-jonollil <53558796+natlibfi-jonollil@users.noreply.github.com>
2019-11-27 16:30:13 +0200

    Sru works

--- src/services/sruService.js
+++ src/services/sruService.js
@@ -4,15 +4,15 @@ import createSruClient from '@natlibfi/sru-client';
 import {SRU_URL, SRU_VERSION} from '../config';
 
 // NEEDS MORE? const sruClient = createSruClient({serverUrl: sruURL, version: SRU_VERSION, maximumRecords: 1});
-const client = createSruClient({serverUrl: 'https://sru.api.melinda-test.kansalliskirjasto.fi/bib', version: 2, maximumRecords: 1});
+const client = createSruClient({serverUrl: 'https://sru.api.melinda-test.kansalliskirjasto.fi/bib', version: '2.0', maximumRecords: '1'});
 const {createLogger} = Utils;
 const logger = createLogger();
 
 export async function getLinkedInfo(marcRecord) {
 	await new Promise((resolve, reject) => {
-		client.searchRetrieve('rec.id=9000') // Haku titlellä client.searchRetrieve('dc.title="kivi*"')
+		client.searchRetrieve('dc.title="kivi*"') // Haku titlellä client.searchRetrieve('dc.title="kivi*"'), Haku idllä client.searchRetrieve('rec.id=9000')
 			.on('record', xmlString => {
-				logger.log(xmlString);
+				logger.log('debug', xmlString);
 				// TODO processRecord(xmlString);
 			})
 			.on('end', () => resolve())
